23ff48e6df0a9339f2c48f7d3f54ffc21118f68f
antdickinson <antdickinsoncreativeprojects@gmail.com>
2025-09-10 15:49:06 +0100

    error fixed

--- public/js/display.js
+++ public/js/display.js
@@ -34,7 +34,7 @@ function setup() {
 
   console.log(`Loaded ${spriteSheets.length} sprite sheets and animations`);
   // Create canvas using default settings (will be updated when clients send settings)
-  let canvas = createCanvas(1080, 1080); //here
+  let canvas = createCanvas(settings.canvas.displayWidth, settings.canvas.displayHeight); //here
   canvas.parent("canvasContainer");
 
   // Set background
