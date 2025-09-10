2de56fee47e5dbcc9852a336237eafd4247f3f6d
antdickinson <antdickinsoncreativeprojects@gmail.com>
2025-09-10 15:38:17 +0100

    Update display2.js

--- public/js/display2.js
+++ public/js/display2.js
@@ -35,7 +35,7 @@ function setup() {
   console.log(`Loaded ${spriteSheets.length} sprite sheets and animations`);
   // Create canvas using default settings (will be updated when clients send settings)
   let canvas = createCanvas(1920, 1080); // Default size, will be updated
-  canvas.parent("canvasContainer");
+  canvas.parent("canvasContainer2");
 
   // Set background
   background(settings.canvas.displayBackgroundColor);
